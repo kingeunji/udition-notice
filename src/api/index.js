82d9e83895b873e205b528ae28142ec29067f6b3
sunghaeJoung <shh03288@gmail.com>
2020-03-24 12:41:19 +0900

    초기세팅

---
+++ src/api/index.js
@@ -0,0 +1,11 @@
+// import axios from 'axios'
+
+// const instance = axios.create({
+//     baseURL: "",
+// });
+
+// export const (urlname) = {
+//     list() {
+//         return instance.(호출방식)('나머지 주소', {request})
+//     }
+// }
